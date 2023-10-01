a9413c05d7051bb3fcc8c4296e27fb0b16439cb1
Adriana Reyes <areye2020@gmail.com>
2023-10-01 12:14:10 -0500

    try5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated import
+import { Route, Routes } from 'react-router-dom'; // Updated import
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Resume from './components/Resume';
@@ -8,19 +8,17 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Navbar />
-        <Routes> {/* Updated to use Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/personal-journey" element={<PersonalJourney />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* Add the following route to ensure Home is rendered when first loaded */}
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="app">
+      <Navbar />
+      <Routes> {/* Updated to use Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/personal-journey" element={<PersonalJourney />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* Add the following route to ensure Home is rendered when first loaded */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
